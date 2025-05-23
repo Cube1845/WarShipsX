29199af15a169d18807595c02fa6f9e08bb50bac
Cube <wieszalajakub@gmail.com>
2025-05-23 23:33:57 +0200

    initial frontend commit

---
+++ warships-x/android/app/src/main/java/com/cube1845/warshipsx/MainActivity.java
@@ -0,0 +1,5 @@
+package com.cube1845.warshipsx;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ warships-x/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.warshipsx.app',
+  appName: 'Warships X',
+  webDir: 'dist/warships-x/browser',
+};
+
+export default config;
